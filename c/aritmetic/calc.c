e66b19055b462622726e9b44aef153a92dae35da
jangulo <jangulo@v.fitbit.com>
2019-01-29 15:36:37 -0600

    sketch of aritmetic calculator of big numbers

---
+++ c/aritmetic/calc.c
@@ -0,0 +1,24 @@
+#include "aritmetic.h"
+#include <stdio.h>
+
+
+int main()
+{
+    uint8_t xNum[] = {1,2,3,4};
+    uint8_t yNum[] = {5,6,7,8};
+
+    stBigNum x = {  .number = xNum,
+                    .digitNum = 4,
+                    .strRepresentation = "1234"
+                };
+
+    stBigNum y = {  .number = yNum,
+                    .digitNum = 4,
+                    .strRepresentation = "5678"
+                };
+
+    printf("x = %s ", x.strRepresentation);
+    printf("y = %s \n", y.strRepresentation);
+
+    return 0;
+}
