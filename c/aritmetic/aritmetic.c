6e229ad334c18172d9676f36930d1b10194fa30c
jangulo <jangulo@v.fitbit.com>
2019-01-29 15:36:02 -0600

    template of implementation of sum of big numbers

---
+++ c/aritmetic/aritmetic.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "aritmetic.h"
+
+
+stBigNum * aritSum(stBigNum * x, stBigNum * y)
+{
+    if ( y != NULL && x != NULL) 
+    {
+        size_t nDigits = x->digitNum > y->digitNum ? : x->digitNum;
+        uint8_t carry = 0;
+        for(size_t i = 0; i < nDigits; i++)
+        {
+            // TODO
+        }
+        
+    }
+    
+    return NULL;
+}
