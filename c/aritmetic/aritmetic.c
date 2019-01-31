82e1b22499d6ceb0446b733660e783a280e72ccf
jangulo <jangulo@v.fitbit.com>
2019-01-31 08:22:43 -0600

    still working in regular sum of big numbers

--- c/aritmetic/aritmetic.c
+++ c/aritmetic/aritmetic.c
@@ -1,26 +1,46 @@
-#include <stdlib.h>
+#include <string.h>
 #include "aritmetic.h"
 
 
 stBigNum* aritBigNumInit(size_t digitNum)
 {
+    return NULL;
+}
 
+ErrorCode aritBigNumFromStr(stBigNum *num, char * repr)
+{
+    while(*repr != '\0')
+    {
+        
+    }
+
+    return OK;
 }
 
 stBigNum * aritSum(stBigNum * x, stBigNum * y)
 {
-    stBigNum * result = NULL;
+    stBigNum * pResult = NULL;
 
     if ( y != NULL && x != NULL) 
     {
-        result = (stBigNum *) malloc(sizeof(stBigNum));
-        size_t nDigits = x->digitNum > y->digitNum ? : x->digitNum;
+        pResult = (stBigNum *) malloc(sizeof(stBigNum));
+        uint8_t * pNumber = (uint8_t*) malloc(sizeof(MAX(x->digitNum, y->digitNum) + 1));
+        size_t nDigits = x->digitNum > y->digitNum ? x->digitNum : x->digitNum;
+        
         uint8_t carry = 0;
+
         for(size_t i = 0; i < nDigits; i++)
         {
-            // TODO
+            pNumber[i] = x->number[i] + y->number[i];
+           
+            uint8_t currentCarry = pNumber[i]/10;
+            pNumber[i] = (pNumber[i] % 10) + carry;
+
+            carry = currentCarry;
         }
         
+        pResult->number = pNumber;
+
     }
     
     return result;
--- c/aritmetic/aritmetic.h
+++ c/aritmetic/aritmetic.h
@@ -1,7 +1,16 @@
 #ifndef ARITMETIC_H
 #define ARITMETIC_H
 
+#include <stdlib.h>
 #include <stdint.h>
+
+#define MAX(x, y) ( (x) > (y) ? (x) : (y) )
+
+typedef enum error{
+    OK = 0,
+    INVALID_STR_REPR = 1
+    
+}ErrorCode;
 
 typedef struct _stBigNum
 {
@@ -9,6 +18,8 @@ typedef struct _stBigNum
     size_t digitNum;
     char * strRepresentation;
 }stBigNum;
+
+ErrorCode aritBigNumFromStr(stBigNum *num, char * repr);
 
 stBigNum* aritBigNumInit(size_t digitNum);
 stBigNum * aritSum(stBigNum * x, stBigNum * y);
