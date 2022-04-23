8f7651381d422e59aaa27f0ebb880d530faa2eb8
joas77 <solojoas@gmail.com>
2022-04-22 23:03:09 -0500

    Added test files and logic encapsulated in class
    In order to count comparisions

--- cpp/sort/quickSort.h
+++ cpp/sort/quickSort.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <iostream>
 #include <algorithm>
 
-template<typename Iterator>
-Iterator partition(Iterator itLeft, Iterator itRight)
+template <typename RandomAccessIterator>
+class QuickSorter
+{
+public:
+    RandomAccessIterator partition(RandomAccessIterator itLeft, RandomAccessIterator itRight);
+    void sort(RandomAccessIterator begin, RandomAccessIterator end);
+
+private:
+    int m_cmpCount = 0; 
+    void quicksort(RandomAccessIterator begin, RandomAccessIterator end);
+    RandomAccessIterator choosePivot(RandomAccessIterator begin, RandomAccessIterator end);
+};
+
+template<typename RandomAccessIterator>
+RandomAccessIterator QuickSorter<RandomAccessIterator>::partition(RandomAccessIterator itLeft, RandomAccessIterator itRight)
 {
     auto p = *itLeft;
     auto it = itLeft;
@@ -10,6 +24,7 @@ Iterator partition(Iterator itLeft, Iterator itRight)
 
     for (auto jit = it; jit != itRight; ++jit)
     {
+        m_cmpCount++;
         if(*jit < p)
         {
             std::swap(*jit, *it);
@@ -24,20 +39,37 @@ Iterator partition(Iterator itLeft, Iterator itRight)
 }
 
 // Naive implementation
-template <typename Iterator>
-Iterator choosePivot(Iterator begin, Iterator end)
+template <typename RandomAccessIterator>
+RandomAccessIterator QuickSorter<RandomAccessIterator>::choosePivot(RandomAccessIterator begin, RandomAccessIterator end)
 {
     return begin;
 }
 
 template <typename RandomAccessIterator>
-void quicksort(RandomAccessIterator begin, RandomAccessIterator end)
+void QuickSorter<RandomAccessIterator>::quicksort(RandomAccessIterator begin, RandomAccessIterator end)
 {
-    if (begin>=end) return;             // 0 or 1 element subarray
+    if (begin>=end)
+    {
+        return;      // 0 or 1 element subarray
+    }
+    
     auto it = choosePivot(begin, end);
     std::swap(*begin, *it);             // make pivot first
 
     auto jit = partition(begin, end);   // jit = new pivot position
+
+    //m_cmpCount += std::distance(begin, jit - 1) +1;
+    //m_cmpCount += std::distance(jit + 1, end) +1;
     quicksort(begin, jit - 1);
     quicksort(jit +  1, end);
+}
+
+template <typename RandomAccessIterator>
+void QuickSorter<RandomAccessIterator>::sort(RandomAccessIterator begin, RandomAccessIterator end)
+{
+    m_cmpCount = 0;
+    quicksort(begin, end);
+
+    std::cout << "total of comparisions = " << m_cmpCount << std::endl;
+    m_cmpCount = 0;
 }
--- cpp/sort/test.cpp
+++ cpp/sort/test.cpp
@@ -1,40 +1,68 @@
 #include <iostream>
 #include <list>
+#include <string>
 #include "quickSort.h"
 #include "utils.h"
 
+template <typename T>
+void runSortTest(std::vector<T>& v, const std::string& msg)
+{
+    QuickSorter<std::vector<int>::iterator> sorter;
+    std::cout << "RUNNING SORT TEST ..." << std::endl;
+    std::cout << msg << std::endl;
+    std::cout << "Before to sort ..." << std::endl;
+    printVector(v);
+    sorter.sort(v.begin(), v.end());
+    std::cout << "After sort ..." << std::endl;
+    printVector(v);
+    std::cout << "****************************" << std::endl;
+}
+
+// template <typename T>
+// void runPartitionTest(std::vector<T>& v, const std::string& msg)
+// {
+//      std::cout << "RUNNING PARTITION TEST ..." << std::endl;
+//     std::cout << msg << std::endl;
+//     std::cout << "Before to partition ..." << std::endl;
+//     printVector(v);
+//     partition(v.begin(), v.end());
+//     printVector(v);
+//     std::cout << "****************************" << std::endl;
+// }
+
 int main()
 {
-    auto initList = std::initializer_list<int>{3,8,2,5,1,4,7,6};
+    auto initList = std::initializer_list<int>{3,8,2,5,1,4,7,6, 10, 9};
     auto v = std::vector<int>{initList};
     auto w = v;
-    std::cout << "Testing partition with std::vector, pivot = first element ("
-        << v.at(0) << ")" << std::endl;
-    printVector(v);
-    partition(v.begin(), v.end());
-    printVector(v);
-    std::cout << "****************************" << std::endl;
+    //runPartitionTest(v, "Testing partition with std::vector, pivot = first element");
 
-    std::cout << "Testing quicksort with std::vector" <<std::endl;
-    quicksort(v.begin(), v.end());
-    printVector(v);
-    std::cout << "****************************" << std::endl;
+    runSortTest(v, "Testing quicksort with std::vector");
 
     auto l  = std::list<int>{initList};
-    std::cout << "Testing partition with std::list, pivot = first element ("
-        << *l.begin() << ")" << std::endl;
-    printIterable(l.begin(), l.end());
-    partition(l.begin(), l.end());
-    printIterable(l.begin(), l.end());
-    std::cout << "****************************" << std::endl;
+    // std::cout << "Testing partition with std::list, pivot = first element ("
+    //     << *l.begin() << ")" << std::endl;
+    // printIterable(l.begin(), l.end());
+    // partition(l.begin(), l.end());
+    // printIterable(l.begin(), l.end());
+    // std::cout << "****************************" << std::endl;
 
-    std::cout << "Testing quicksort with std::list" <<std::endl;
-    std::cout << "before sort..." << std::endl;
-    printIterable(w.begin(), w.end());
-    quicksort(w.begin(), w.end());
-    std::cout << "after sort..." << std::endl;
-    printIterable(w.begin(), w.end());
-    std::cout << "****************************" << std::endl;
+    // std::cout << "Testing quicksort with std::list" <<std::endl;
+    // std::cout << "before sort..." << std::endl;
+    // printIterable(w.begin(), w.end());
+    // quicksort(w.begin(), w.end());
+    // std::cout << "after sort..." << std::endl;
+    // printIterable(w.begin(), w.end());
+    // std::cout << "****************************" << std::endl;
+
+    int data;
+    std::vector<int> vTest;
+    while(std::cin>>data)
+    {
+        vTest.push_back(data);
+    }
+
+    runSortTest(vTest, "Sorting elements from stdin...");
     
     return 0;
 }
